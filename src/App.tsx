890622c736f46b1e504c87bf080ef6eb4e3101b2
rektDev <rektdevvv@gmail.com>
2022-10-01 19:20:37 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./styles.css";
 import Navigation from "./components/Navigation";
+import SaladMaker from "./components/SaladMaker";
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <Navigation />
-      <h2>Start fggfg to see some magic happen!</h2>
+      <SaladMaker />
     </div>
   );
 }
