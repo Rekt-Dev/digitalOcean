aced0234fa492514305f941a7c8b36f17fa6fb2c
rektDev <rektdevvv@gmail.com>
2022-10-01 19:14:12 +0000

    Update 3 files

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,1 +1,15 @@
-export const Navigation = () => {};
+import React from "react";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  wrapper: {
+    borderBottom: "black solid 1px",
+    padding: [15, 10],
+    textAlign: "right",
+  },
+});
+
+export default function Navigation() {
+  const classes = useStyles();
+  return <div className={classes.wrapper}>Welcome, 0Fear</div>;
+}
