f545c8ef682c4dba066a83dd2e18ff47c2cb8eca
rektDev <rektdevvv@gmail.com>
2022-10-03 18:55:29 +0000

    Update Product.jsx

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import './Product.css';
+import React from "react";
+import "./Product.css";
 
 export default function Product() {
-  return(
+  return (
     <div className="wrapper">
-      <div>
-        Shopping Cart: 0 total items.
-      </div>
+      <div>Shopping Cart: 0 items.</div>
       <div>Total: 0</div>
-
-      <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
+      <div className="product">
+        <span role="img" aria-label="ice cream">
+          🍦
+        </span>
+      </div>
       <button>Add</button> <button>Remove</button>
     </div>
-  )
+  );
 }
